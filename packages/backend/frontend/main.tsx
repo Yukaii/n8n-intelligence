8d6d00a9620d5bee8c975e104924fbd3cef84dcf
Yukai Huang <yukaihuangtw@gmail.com>
2025-04-26 10:33:54 +0800

    refactor: fullstack

--- packages/backend/frontend/main.tsx
+++ packages/backend/frontend/main.tsx
@@ -1,3 +1,10 @@
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<div>Hello World</div>)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
