0bd150167f5f3d57c15dc405850dd510d1f8d606
Yukai Huang <yukaihuangtw@gmail.com>
2025-04-26 16:11:26 +0800

    fix: update routing structure and configuration for improved navigation and organization

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+import { routeTree } from './routeTree.gen'
+
+import "./index.css";
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import path from "path"
+import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true, routesDirectory: 'frontend/routes', generatedRouteTree: 'frontend/routeTree.gen.ts' }),
   react(), cloudflare(), tailwindcss()],
   resolve: {
     alias: {
